9516079cd2403f7b774a7708eef2991432096739
Andrew Kozey <kozeyandrey@gmail.com>
2016-09-21 20:11:17 +0300

    Added "truthiness matches" to exprectation_matches_spec.rb

--- spec/exprectation_matches_spec.rb
+++ spec/exprectation_matches_spec.rb
@@ -38,4 +38,39 @@ describe 'Expectation Matches' do
       expect(c).to be(b) # synonym to #equal
     end
   end
+
+  # be_truthy was formerly knows as be_true (be_true - don't use this variant)
+  # be_falsey was formerly knows as be_false (be_false - don't use this variant)
+  describe 'truthiness matches' do
+    it 'will match true/false' do
+      expect(1 < 2).to be(true) # don't use 'be_true' + passed if expression == true
+      expect(1 > 2).to be(false) # don't use 'be_false' + passed if expression == false
+
+      expect('foo').not_to be(true) # this string is not exactly true
+
+      expect(nil).not_to be(false) # nil is not exactly true
+      expect(0).not_to be(false) # 0 is not exactly true
+
+    end
+
+    it 'will match truthy/falsey' do
+      expect(1 < 2).to be_truthy # passed if expression is truthy (not nil or false)
+      expect(1 > 2).to be_falsey # passed if expression is falsey (nil or false)
+
+      expect('foo').to be_truthy # any value counts as true
+
+      expect(nil).to be_falsey # nil counts as false
+      expect(0).not_to be_falsey  # but 0 is still not falsey enough
+    end
+
+    it 'will match truthy/falsey' do
+      expect(nil).to be_nil
+      expect(nil).to be(nil) # either way works
+
+      expect(false).not_to be_nil # nil only, just like #nil?
+
+      expect(0).not_to be_nil # nil only, just like #nil?
+    end
+
+  end
 end
