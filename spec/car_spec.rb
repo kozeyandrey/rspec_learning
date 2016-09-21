a9e538ea62e1f016fb629fc70a8cbb826964d8de
Andrew Kozey <kozeyandrey@gmail.com>
2016-09-21 13:42:15 +0300

    Added skip to test "allows reading and writing for :make"

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,7 +2,7 @@ require 'car'
 
 describe 'Car' do
 	describe 'attributes' do
-		it 'allows reading and writing for :make' do
+		xit 'allows reading and writing for :make' do
 			car = Car.new
 			car.make = 'Test'
 			expect(car.make).to eq('Test')
